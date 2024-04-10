7f7eab554abd6f83dfb20f52013c3e5adfacc9fe
BurritoLewis <bartoolewis@gmail.com>
2024-04-10 09:58:13 +0300

    Abstracted the BufferLayout .

--- Opengl_tutorial/res/shaders/Basic.glsl
+++ Opengl_tutorial/res/shaders/Basic.glsl
@@ -2,24 +2,33 @@
 #version 330 core
 
 layout(location = 0) in vec4 position;
-       
+layout(location = 1) in vec4 color;
+
+out vec4 colour;
 
 void main()
 {
 gl_Position = position;
+colour = color;
 };
+
+
 
 
 
 //#shader fragment
 #version 330 core
 
+
+
 layout(location = 0) out vec4 color;
 
 uniform vec4 u_Color;
 
+in vec4 colour;
+
 void main()
 {
-    color = u_Color;
+    color = colour;
 };
     
--- Opengl_tutorial/src/Application.cpp
+++ Opengl_tutorial/src/Application.cpp
@@ -10,6 +10,7 @@
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexArray.h"
 
 struct ShaderProgramSource
 {
@@ -124,18 +125,17 @@ int main(void)
   
     glfwSwapInterval(1);
 
-    std::cout << "oggk man" << std::endl;
 
     if (glewInit() != GLEW_OK)
-        std::cout << "Glew Error" << std::endl;
+        std::cout << "glue Error" << std::endl;
 
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
         float positions[] = {
-            -0.5f, -0.5f,
-             0.5f, -0.5f,
-             0.5f,  0.5f,
-            -0.5f,  0.5f,
+            -0.5f, -0.5f,  1.0f,0.0f,0.0f,1.0f,
+             0.5f, -0.5f,  0.0f,1.0f,0.0f,1.0f,
+             0.5f,  0.5f,  0.0f,0.0f,1.0f,1.0f,
+            -0.5f,  0.5f,  1.0f,1.0f,1.0f,1.0f
         };
 
         unsigned int indecies[] = {
@@ -147,10 +147,14 @@ int main(void)
         GLCall(glGenVertexArrays(1, &vao));
         GLCall(glBindVertexArray(vao));
 
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexArray va;
+        VertexBuffer vb(positions, 4 * 6 * sizeof(float));
 
-        GLCall(glEnableVertexAttribArray(0));
-        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+        VertexBufferLayout layout;
+		layout.Push<float>(2);
+		layout.Push<float>(4);
+        va.AddBuffer(vb,layout);
+        
 
         IndexBuffer ib(indecies, 6);
 
@@ -160,7 +164,7 @@ int main(void)
         GLCall(glUseProgram(shader));
 
         GLCall(int location = glGetUniformLocation(shader, "u_Color"));
-        ASSERT(location != -1);
+        //ASSERT(location != -1);
 
         GLCall(glBindVertexArray(0));
         GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
@@ -183,7 +187,7 @@ int main(void)
             GLCall(glUseProgram(shader));
             GLCall(glUniform4f(location, r, 0.0f, 1.0f, 1.0f));
 
-            GLCall(glBindVertexArray(vao));
+            va.Bind();
             ib.Bind();
 
             GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
